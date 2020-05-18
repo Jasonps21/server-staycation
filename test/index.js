31cc66c70142992416e566302af6b344f392a9d0
unknown <jpsunarji21@gmail.com>
2020-05-18 08:59:44 +0800

    first commit

---
+++ test/index.js
@@ -0,0 +1,129 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const app = require("../app");
+const fs = require("fs");
+
+chai.use(chaiHttp);
+
+describe("Api endpoint testing", () => {
+  it("get landing page", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/member/landing-page")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("Object");
+        expect(res.body).to.have.property("hero");
+        expect(res.body.hero).to.have.all.keys(
+          "travelers",
+          "treasures",
+          "cities"
+        );
+        expect(res.body).to.have.property("hero");
+        expect(res.body.mostPicked).to.have.an("array");
+        expect(res.body).to.have.property("category");
+        expect(res.body.category).to.have.an("array");
+        expect(res.body).to.have.property("testimonial");
+        expect(res.body.testimonial).to.have.an("Object");
+        done();
+      });
+  });
+
+  it("get detail page", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/member/detail-page/5e96cbe292b97300fc902223")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("Object");
+        expect(res.body).to.have.property("country");
+        expect(res.body).to.have.property("isPopular");
+        expect(res.body).to.have.property("unit");
+        expect(res.body).to.have.property("sumBooking");
+        expect(res.body).to.have.property("_id");
+        expect(res.body).to.have.property("title");
+        expect(res.body).to.have.property("price");
+        expect(res.body).to.have.property("city");
+        expect(res.body).to.have.property("description");
+        expect(res.body).to.have.property("imageId");
+        expect(res.body.imageId).to.have.an("array");
+        expect(res.body).to.have.property("featureId");
+        expect(res.body.featureId).to.have.an("array");
+        expect(res.body).to.have.property("activityId");
+        expect(res.body.activityId).to.have.an("array");
+        expect(res.body).to.have.property("bank");
+        expect(res.body.bank).to.have.an("array");
+        expect(res.body).to.have.property("testimonial");
+        expect(res.body.testimonial).to.have.an("Object");
+        done();
+      });
+  });
+
+  it("post booking page", (done) => {
+    const image = __dirname + "/bukti.jpeg";
+    const dataSample = {
+      image,
+      duration: 2,
+      bookingStartDate: "9-4-2020",
+      bookingEndDate: "11-4-2020",
+      firstName: "Jason",
+      lastName: "Pratama",
+      email: "jpsunarji@gmail.com",
+      phoneNumber: "09023123132",
+      accountHolder: "Jason",
+      bankFrom: "BCA",
+      idItem: "5e96cbe292b97300fc902223",
+    };    
+    chai
+      .request(app)
+      .post("/api/v1/member/booking-page")
+      .set("Content-Type", "application/x-www-form-urlencoded")
+      .attach("image", fs.readFileSync(image), "/test/bukti.jpeg")
+      .field("idItem", dataSample.idItem)
+      .field("duration", dataSample.duration)
+      .field("bookingStartDate", dataSample.bookingStartDate)
+      .field("bookingEndDate", dataSample.bookingEndDate)
+      .field("firstName", dataSample.firstName)
+      .field("lastName", dataSample.lastName)
+      .field("email", dataSample.email)
+      .field("phoneNumber", dataSample.phoneNumber)
+      .field("accountHolder", dataSample.accountHolder)
+      .field("bankFrom", dataSample.bankFrom)      
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("message");
+        expect(res.body.message).to.equal("Success Booking");
+        expect(res.body).to.have.property("booking");
+        expect(res.body.booking).to.have.all.keys(
+          "payments",
+          "_id",
+          "invoice",
+          "bookingStartDate",
+          "bookingEndDate",
+          "total",
+          "itemId",
+          "memberId",
+          "__v"
+        );
+        expect(res.body.booking.payments).to.have.all.keys(
+          "status",
+          "proofPayment",
+          "bankFrom",
+          "accountHolder"
+        );
+        expect(res.body.booking.itemId).to.have.all.keys(
+          "_id",
+          "title",
+          "price",
+          "duration"
+        );
+        console.log(res.body.booking);
+        done();
+      });
+  });
+});
